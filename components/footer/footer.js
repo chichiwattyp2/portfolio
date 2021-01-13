a30977f2c1657ba62702a16d367e24ca38c5f662
Glitch (diagnostic-crystal-krypton) <none>
2021-01-13 01:46:29 +0000

    🎇🌑 Updated with Glitch

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,92 +1,310 @@
 const FooterContent = `
   <style>
-    #footer{
-      padding: 4em 12vw;  
+   
+    font-family: "ClobberGroteskW01-Medium";
+src: url("//db.onlinewebfonts.com/t/f4e31ff864f4297e98cd95bd1d07fd7b.eot");
+src: url("//db.onlinewebfonts.com/t/f4e31ff864f4297e98cd95bd1d07fd7b.eot?#iefix")
+    format("embedded-opentype"),
+  url("//db.onlinewebfonts.com/t/f4e31ff864f4297e98cd95bd1d07fd7b.woff2")
+    format("woff2"),
+  url("//db.onlinewebfonts.com/t/f4e31ff864f4297e98cd95bd1d07fd7b.woff")
+    format("woff"),
+  url("//db.onlinewebfonts.com/t/f4e31ff864f4297e98cd95bd1d07fd7b.ttf")
+    format("truetype"),
+  url("//db.onlinewebfonts.com/t/f4e31ff864f4297e98cd95bd1d07fd7b.svg#ClobberGroteskW01-Medium")
+    format("svg");
 
-          }
+font-family: "grifterbold";
+src: url("https://beta-supercruisr.neocities.org/assets/css/grifterbold-webfont.woff2")
+    format("woff2"),
+  url("https://beta-supercruisr.neocities.org/assets/css/grifterbold-webfont.woff")
+    format("woff");
+font-weight: normal;
+font-style: normal;
 
-    .lead {
-      font-family: Chakra Petch;
-      font-style: normal;
-      font-weight: bold;
-      font-size: 1.75em;
-      line-height: 140%;
-      color: white;
-    }
 
-    .buttons > button,
-    .buttons > a {
-        margin-right: 3.125em;
-        cursor: pointer;
-    }
+/* Typography */
+.title {
+  font-family: ClobberGroteskW01-Medium;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 3em;
+}
 
-    .paragraph {
-        font-family: Source Sans Pro;
-        max-width: 31.25em;
-        color: white;
-    }
+.lead {
+  font-family: ClobberGroteskW01-Medium;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1.75em;
+  line-height: 140%;
+}
 
-    .primary-button,
-    .passive-button {
-      display: inline-block;
-      font-family: Chakra Petch;
-      font-style: normal;
-      font-weight: 600;
-      font-size: 1em;
-      line-height: 1.25em;
-      height: 3.5em;
-      border: none;
-      border: 1.5px solid black;
-      box-sizing: border-box;
-      box-shadow: 10px 10px 0px black;
-      border-radius: 10px;
-      padding: 1em 1.25em;
-      margin: 1.875em 0;
-      text-decoration: none;
-      color: black;
-    }
+.paragraph,
+.small,
+.link,
+.error {
+  font-family: ClobberGroteskW01-Medium;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 140%;
+}
 
-    .primary-button:active,
-    .passive-button:active {
-      box-shadow: 5px 5px 0px #000000;
-      transform: translateY(4px);
-    }
+.paragraph {
+  font-size: 1em;
+}
 
-    .primary-button {
-      background: var(--primary-color);
-    }
+.small {
+  font-size: 0.875em;
+}
 
-    .primary-button:hover {
-      background: var(--primary-color-dark);
-    }
+.link {
+  color: var(--link-color);
+  text-decoration: none;
+  font-weight: bold;
+}
 
-    .passive-button {
-      background: var(--passive-color);
-    }
+.error {
+  font-style: italic;
+  font-size: 1em;
+  color: #df0000;
+}
 
-    .passive-button:hover {
-      background: var(--passive-color-dark);
-    }
+/* Buttons */
+.primary-button,
+.secondary-button,
+.passive-button {
+  display: inline-block;
+  font-family: ClobberGroteskW01-Medium;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1em;
+  line-height: 1.25em;
+  height: 3.5em;
+  border: none;
+  border: 1px solid #111;
+  box-sizing: border-box;
 
-    .buttons > .publish-disabled {
-        cursor: default;
-        opacity: 0.4;
-    }
-    /* Responsive */
+  padding: 1em 1.25em;
+  margin: 1.875em 0;
+  text-decoration: none;
+  color: #111;
+  cursor: pointer;
+}
+
+.primary-button:active,
+.secondary-button:active,
+.passive-button:active {
+  transform: translateY(4px);
+}
+
+.primary-button {
+  background: transparent;
+}
+
+.primary-button:hover {
+  background: transparent;
+}
+
+.secondary-button {
+  background: transparent;
+}
+
+.secondary-button:hover {
+  background: transparent;
+}
+
+.passive-button {
+  background: transparent;
+}
+
+.passive-button:hover {
+  background: transparent;
+}
+
+.cursor-pointer {
+  cursor: pointer;
+}
+
+/* body */
+body {
+  margin: 0;
+}
+
+hr {
+  margin: 2.25em 0.625em;
+  border: 1px solid var(--passive-color);
+}
+
+
+
+page-footer {
+  position: absolute;
+  
+  width: 100%;
+}
+
+
+.pages-content {
+  font-family: Source Sans Pro;
+  padding: 4em 12vw;
+}
+
+.pages-content-container {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 2em;
+}
+
+.pages-content-element {
+  width: 50%;
+  padding: 1em;
+  max-width: 30em;
+  margin-right: 1em;
+}
+
+/* TODO: move this to location.css */
+.pages-content-element .lead {
+  margin-top: 0;
+}
+
+/* TODO: move this to location.css */
+.pages-content-container:first-of-type {
+  margin-bottom: 2.25em;
+}
+
+/* TODO: resolve duplicate conflict. move this to the back-anchor component. */
+.back-anchor {
+  padding: 1em 0em;
+  height: 3rem;
+}
+.coordinates-input-wrapper {
+  margin-bottom: 1rem;
+}
+
+.back-anchor {
+  padding: 1em;
+  font-size: 1em;
+}
+
+/* TODO: move .coordinates-input-container to location.css */
+.coordinates-input-container {
+  width: 50%;
+  display: flex;
+  margin-bottom: 1em;
+}
+
+.coordinates-input-container .input-number-tags {
+  padding-left: 0;
+}
+
+.add-location-container {
+  width: fit-content;
+  cursor: pointer;
+}
+
+.coordinates-input-container > div {
+  padding: 1rem;
+  display: flex;
+  width: 50%;
+  flex-flow: column nowrap;
+  justify-content: center;
+  text-align: justify;
+}
+
+.delete-coordinates-container > svg {
+  margin-top: 2.5rem;
+}
+
+.remove-location-container {
+  display: flex;
+  cursor: pointer;
+}
+
+.add-location-container {
+  display: flex;
+}
+
+.add-location-icon {
+  margin-right: 0.4rem;
+}
+
+.input-container label {
+  font-family: Source Sans Pro;
+  font-size: 0.875rem;
+}
+
+.map-buttons-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.text-input {
+  border: 1px solid var(--passive-color-dark);
+  box-sizing: border-box;
+  border-radius: 5px;
+  height: 3.125rem;
+  padding: 1rem 1.25rem;
+  font-family: Source Sans Pro;
+  font-size: 0.875rem;
+  margin-top: 0.5rem;
+}
+
+input:focus {
+  outline: none;
+}
+
+#content-preview {
+  display: flex;
+  flex-direction: column;
+}
+
+.download-default-marker {
+  display: flex;
+  vertical-align: middle;
+  font-style: italic;
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.beta {
+  font-family: Source Sans Pro;
+  margin-top: 5em;
+  background-color: #fff700;
+  padding: 1em;
+}
+
+.repo-url {
+  font-size: 1.25em;
+}
+
+.buttons-container {
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+  padding-right: 2em;
+}
+
+@keyframes blink {
+  50% {
+    color: transparent;
+  }
+}
+.loader__dot {
+  animation: 1s blink infinite;
+}
+.loader__dot:nth-child(2) {
+  animation-delay: 250ms;
+}
+.loader__dot:nth-child(3) {
+  animation-delay: 500ms;
+}
+
+/* Responsive */
 @media screen and (max-width: 900px) {
   /* Typography */
   .title {
     font-size: 2.5em;
   }
-  
-  .lead {
-  width: 80%;
-  
-}
-.paragraph {
-  width: 80%;
-  
-}
 
   .pages-content-container {
     flex-direction: column;
@@ -95,6 +313,11 @@ const FooterContent = `
     width: 100%;
   }
 }
+ #footer{
+      padding: 4em 12vw;  
+
+          }
+
   </style>
 
   <div>
